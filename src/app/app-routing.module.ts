c73a9d6067e11e60ee8d25e790e0a0932328810e
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2019-07-11 14:10:14 -0400

    Initial commit
    - Adds folder structure
    - Adds files for template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
+import { AuthGuard } from './core/guards/auth.guard';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   canActivate: [AuthGuard], // Uncomment to activate user login guard
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
